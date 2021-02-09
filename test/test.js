e57b1ad63c3813865f777d7992175329c4bad77a
Aidan VanLeuven <aidanvanleuven@gmail.com>
2021-02-09 11:42:01 -0700

    Closes #13 (#24)

--- test/test.js
+++ test/test.js
@@ -6,9 +6,9 @@ fs = require('fs');
 // This agent refers to PORT where program is runninng.
 var server = supertest.agent(app);
 
-// Express test, used for verifying test action
+// Make sure that GET / returns the index page
 describe("GET /", function() {
-  it("render index.ejs", function(done) {
+  it("renders index.ejs", function(done) {
     server.get("/")
       .expect("Content-type", /text\/html/)
       .expect(200)
@@ -17,6 +17,25 @@ describe("GET /", function() {
           return done(err);
         }
         res.status.should.equal(200);
+        res.body.should.match(/<h1>Express<\/h1>/);
+        done();
+      });
+  });
+});
+
+// Make sure that /users returns a table of users
+// Make sure to change this in prod
+describe("GET /users", function() {
+  it("render users.ejs", function(done) {
+    server.get("/")
+      .expect("Content-type", /text\/html/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        res.status.should.equal(200);
+        res.body.should.match(/<table id="user-table">/);
         done();
       });
   });
