7562c1d53bc37f0cbc6b4f9ae02972f8bab60537
Aidan <aidanvanleuven@gmail.com>
2021-02-09 11:21:05 -0700

    parse and display data from users.json

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
+
+// Change this for prod
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  fs.readFile('users.json', function (err, data) {
+    if (err) {
+      return res.send(err);
+    }
+
+    var parsed = JSON.parse(data);
+
+    res.locals.users = parsed.users;
+    return res.render("users");
+  });
+  
 });
 
 module.exports = router;
