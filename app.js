8222b814d9b45eed263a0e5e63c545e5bb9618f9
Aidan <aidanvanleuven@gmail.com>
2021-02-23 18:47:19 -0700

    First portion of auth system, needs testing

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('ald-monitor:server');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+
+// For our sessions
+var secret = 'z6[q2MrHb(r_KzS/,Q)cJ:xeP)vyxYR4ASU*bBLVYJ)vE54nwPnJp7hAXyg]mT-@';
 
 var app = express();
 
@@ -16,12 +23,23 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev', { stream: { write: msg => debug(msg) } }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+// Passport setup for express
+app.use(cookieParser());
+app.use(expressSession({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +55,56 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Passport configuration
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password'
+  },
+  function (username, password, done) {
+    fs.readFile('users.json', function (err, data) {
+      if (err) {
+        return res.send(err);
+      }
+  
+      var parsed = JSON.parse(data);
+      var userFound = false;
+      var pwMatch = false;
+
+      parsed.forEach(function (element) {
+        if (element.username == username) {
+          userFound = true;
+          if (element.password == password) {
+            pwMatch = true;
+            return done(null, rows[0], {code : 0, message: "Login success, redirecting..."});
+          }
+        }
+      });
+
+      if (!userFound) {
+        return done(null, false, {code : 1, message: 'Incorrect username.' });
+      }
+
+      if (!pwMatch) {
+        return done(null, false, {code : 2, message: 'Incorrect password.' });
+      }
+    });
+  }
+));
+
+// Passport method to serialize a user
+passport.serializeUser(function (user, done) {
+  done(null, user.UserID);
+});
+
+// Passport method to deserialize a user
+passport.deserializeUser(function (id, done) {
+  pool.execute("SELECT * FROM users WHERE UserID = ?;",
+    [id],
+    function (err, rows) {
+      done(err, rows[0]);
+    });
 });
 
 module.exports = app;
