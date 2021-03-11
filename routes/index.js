9a78704e64932b75d31f85fe26c0169becaa897d
Aidan <aidanvanleuven@gmail.com>
2021-03-11 09:49:49 -0700

    reduce code repetition

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var cel = require('connect-ensure-login');
 var fs = require('fs');
+var parse = require('../parse');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,81 +12,9 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/dashboard', cel.ensureLoggedIn("/login"), function (req, res) {
+  res.locals.data = parse();
+  res.render('dashboard');
 
-  fs.readFile('data_json/data.json', function (err, data) {
-    var jsonData = data;
-
-    // Parse and manipulate the data for use by EJS
-    var parsed = JSON.parse(jsonData, function (key, value) {
-
-      // Add pressure unit label
-      if (key.includes("Pressure")) {
-        value += " Torr";
-      }
-
-      // Add percent unit label
-      if (key.includes("Xtal Life")) {
-        value += "%";
-      }
-
-      // Add flow unit label
-      if (key.includes("Flow")) {
-        value += " sccm";
-      }
-
-      // Add mass unit label
-      if (key.includes("Mass")) {
-        value += " ng/cm²";
-      }
-
-      // Add temp unit label
-      if (key.includes("Thermocouple")) {
-        value += " ºC";
-      }
-
-      // Convert dates and add seconds unit label
-      if (key.includes("Time")) {
-        if (key.includes("Timestamp")) {
-          if (value != "NaN") {
-            var date = new Date(value);
-            value = date.toLocaleDateString("en-US") + " " + date.toLocaleTimeString("en-US");
-          }
-        } else {
-          value += "s";
-        }
-      }
-
-      // Convert duration to time units
-      if (key.includes("Duration")) {
-        value += "s";
-      }
-
-      // Add freq unit label
-      if (key.includes("Frequency")) {
-        value = value.toLocaleString("en-US");
-        value += " Hz";
-      }
-
-      // Mutate 1 or 0 into "yes" or "no"
-      if (key == "Xtal OK?") {
-        if (value == 0) {
-          value = "No";
-        } else {
-          value = "Yes";
-        }
-      }
-
-      // Convert "NaN" to "N/A"
-      if (typeof (value) === 'string' && value.includes("NaN")) {
-        value = "N/A";
-      }
-
-      return value;
-    });
-
-    res.locals.data = parsed;
-    res.render('dashboard');
-  });
 });
 
 router.get('/login', function (req, res) {
