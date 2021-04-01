ff027c505028319dadc59aebf40e84919a4b885c
Aidan <aidanvanleuven@gmail.com>
2021-04-01 10:37:43 -0600

    basic interface improvements and more efficient storage for graphs

--- socketapi.js
+++ socketapi.js
@@ -16,16 +16,16 @@ chokidar.watch('./data_json/data.json', {
   // Parse the singular entry json data
   parse("data_json/data.json", function (single) {
 
-    // Send an update event to all connected clients
-    io.emit("update", single);
-
     // Update historical data for any client that connects later
     parse("data_json/past_data.json", function (many) {
 
       // Don't add the same entry twice
-      if (many.data_log.length == 0 || many.data_log[many.timestamp.length - 1].Timestamp !== single.data_log[0].Timestamp) {
+      if (many.data_log.length == 0 || many.data_log[many.data_log.length - 1].Timestamp !== single.data_log[0].Timestamp) {
 
-        // Store up to 24 data entries (2 minutes)
+        // Send an update event to all connected clients
+        io.emit("update", single);
+
+        // Store up to 360 data entries (30 minutes)
         if (many.timestamp.length >= 360) {
           many.data_log.shift();
           many.exe_table.shift();
@@ -34,12 +34,20 @@ chokidar.watch('./data_json/data.json', {
           many.timestamp.shift();
         }
 
+        // Build object to send
+        var newEntry = {
+          "Timestamp": single.data_log[0].Timestamp,
+          "Chamber Pressure": single.data_log[0]["Chamber Pressure"],
+          "MFC 1 Flow": single.data_log[0]["MFC 1 Flow"],
+          "MFC 2 Flow": single.data_log[0]["MFC 2 Flow"],
+          "MFC 3 Flow": single.data_log[0]["MFC 3 Flow"],
+          "MFC 4 Flow": single.data_log[0]["MFC 4 Flow"],
+          "MFC 5 Flow": single.data_log[0]["MFC 5 Flow"]
+        };
+
         // Push the new data into an array
-        many.data_log.push(single.data_log[0]);
-        many.exe_table.push(single.exe_table[0]);
-        many.doses_delta.push(single.doses_delta[0]);
-        many.summary.push(single.summary);
-        many.timestamp.push(single.timestamp);
+        many.data_log.push(newEntry);
+
 
         // Convert the object into a string
         jsonString = JSON.stringify(many, null, 4);
